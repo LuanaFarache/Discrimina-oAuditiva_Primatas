69b54cf4a656ccd61dae2d942c7d712c9078edbc
LuanaFarache <89166243+LuanaFarache@users.noreply.github.com>
2021-09-20 14:55:09 -0300

    Create README.py

---
+++ README.py
@@ -0,0 +1,6 @@
+# DiscriminacaoAuditiva_Primatas
+'''
+O presente programa tem por objetivo sistematizar um método de discriminação 
+de estímulos auditivos para primatas através do condicionamento operante.
+'''
+
